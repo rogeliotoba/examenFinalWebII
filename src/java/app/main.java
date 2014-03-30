78e0e03e0285063ec709fcc48fc5647c1dd73ec9
Armando Ferreira Lee Eng <lee.eng.a@gmail.com>
2014-03-30 16:17:12 -0600

    Prueba Conexion a Microsoft SQL Server
    Prueba para comprobar que una maquina externa a donde esta instalado el
    MS SQL server se conecte.

--- src/java/app/main.java
+++ src/java/app/main.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import mikitl.Database;
 
 /**
  *
@@ -30,7 +31,8 @@ public class main extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        Database db = new Database ();
+        db.CloseConnection ();
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/main_view.jsp");
         rd.forward(request, response);
     }

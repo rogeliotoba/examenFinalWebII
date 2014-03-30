f64c526fd4d3ccacde357052bb9797b5c32e467e
Armando Ferreira Lee Eng <lee.eng.a@gmail.com>
2014-03-30 15:00:25 -0600

    Se agrego ExecUpdate en la Clase mikitl.Database. Aparte se incluyeron los javadocs para explicar el uso de los metodos en dicha clase.

--- src/java/mikitl/Database.java
+++ src/java/mikitl/Database.java
@@ -39,6 +39,10 @@ public class Database
         }
     }
 
+    /**
+     * Returns void. Este metodo solo cierra la conexion actual hacia Microsoft
+     * SQL Server.
+     */
     public void CloseConnection ()
     {
         try
@@ -51,6 +55,26 @@ public class Database
         }
     }
 
+    /**
+     * Retorna ResultSet que se puede utilizar para analairzar todos los datos
+     * extraidos de la Base de Datos. 
+     * <p>
+     * Este metodo se utlizara para ejecutar los Querys en la Base de Datos. 
+     * Se manda el Querya ejecutar mediante un String, este puede hacer uso de 
+     * comodines '?', pues el Query se pasa a un PreparedStatement. Los argumentos 
+     * se pasan por medio de un Arreglo de Objetos en el orden que se quieren remplazar.
+     * <p>
+     * En caso de que el Query y los Argumentos sean mandados de forma
+     * incorrecta retornara un NULL.
+     *
+     * @param query String a ejecutar en un PreparedStatement. Se puede utilizar
+     * comodines '?'.
+     *
+     * @param args Object[] a remplazar por comodines. Se envian en el orden que
+     * se quiere remplazar, en caso de no usar comodines enviar un NULL.
+     *
+     * @return Un ResultSet de la ejecucion del Query.
+     */
     public ResultSet ExecQuery ( String query, Object[] args )
     {
         PreparedStatement ps = null;
@@ -75,4 +99,53 @@ public class Database
 
         return rs;
     }
+
+    /**
+     * Retorna un TRUE si el update se ejecuto de manera correcta, un FALSE
+     * si se ejecuto de manera incorrecta.
+     * <p>
+     * Este metodo se utilizar para poder ejecutar INSERT, UPDATE y DELETE a
+     * las tablas de la base de datos. Se manda la Sentencia SQL a ejecutar mediante
+     * un String, este puede hacer uso de comodines '?', pues la Sentencia SQL se pasa a un
+     * PreparedStatement. Los argumentos se pasan por medio de un Arreglo de Objetos 
+     * en el orden que se quieren reemplazar.
+     * <p>
+     * En caso de que la Sentencia SQL y los Argumentos sean mandados de forma
+     * incorrecta retornara un NULL.
+     *
+     * @param query String a ejecutar en un PreparedStatement. Se puede utilizar
+     * comodines '?'.
+     *
+     * @param args Object[] a remplazar por comodines. Se envian en el orden que
+     * se quiere remplazar, en caso de no usar comodines enviar un NULL.
+     *
+     * @return TRUE si el update se ejecuto de manera correcta. FALSE si se ejecuto
+     * de manera incorrecta.
+     */
+    public boolean ExecUpdate ( String query, Object[] args )
+    {
+        PreparedStatement ps = null;
+
+        try
+        {
+            ps = con.prepareStatement ( query );
+            if ( args != null )
+            {
+                for ( int i = 0; i < args.length; i++ )
+                {
+                    ps.setObject ( i + 1, args[i] );
+                }
+            }
+            ps.executeUpdate ();
+
+            return true;
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace ();
+            return false;
+        }
+        
+    }
+
 }

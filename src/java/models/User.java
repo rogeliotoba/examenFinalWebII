d60aa4b119301c8dc491363b8a901344693e431c
Armando Ferreira Lee Eng <lee.eng.a@gmail.com>
2014-03-30 20:04:29 -0600

    Se modifico la clase entidad Users

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -8,96 +8,136 @@ package models;
  *
  * @author rogeliotorres
  */
-public class User {
+public class User
+{
+
     private int id;
     private String name;
     private String lastName;
     private String address;
-    private String postalCode;
+    private int postalCode;
     private String phone;
     private String email;
     private String username;
     private String password;
     private int rol;
 
-    public int getId() {
+    public User ()
+    {
+    }
+
+    public User ( int id, String name, String lastName, String address, int postalCode, String phone, String email, String username, String password, int rol )
+    {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public int getId ()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId ( int id )
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName ()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName ( String name )
+    {
         this.name = name;
     }
 
-    public String getLastName() {
+    public String getLastName ()
+    {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName ( String lastName )
+    {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
+    public String getAddress ()
+    {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress ( String address )
+    {
         this.address = address;
     }
 
-    public String getPostalCode() {
+    public int getPostalCode ()
+    {
         return postalCode;
     }
 
-    public void setPostalCode(String postalCode) {
+    public void setPostalCode ( int postalCode )
+    {
         this.postalCode = postalCode;
     }
 
-    public String getPhone() {
+    public String getPhone ()
+    {
         return phone;
     }
 
-    public void setPhone(String phone) {
+    public void setPhone ( String phone )
+    {
         this.phone = phone;
     }
 
-    public String getEmail() {
+    public String getEmail ()
+    {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail ( String email )
+    {
         this.email = email;
     }
 
-    public String getUsername() {
+    public String getUsername ()
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername ( String username )
+    {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPassword ()
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword ( String password )
+    {
         this.password = password;
     }
 
-    public int getRol() {
+    public int getRol ()
+    {
         return rol;
     }
 
-    public void setRol(int rol) {
+    public void setRol ( int rol )
+    {
         this.rol = rol;
     }
-    
+
 }
